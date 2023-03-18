42e0aaec0b40b4409b4bb66f9c66bfa223929956
Maral-IT <amaryllis.l78@gmail.com>
2023-03-18 20:32:37 +0600

    add useState Hook

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
@@ -5,11 +6,49 @@ import Hello from './components/Hello/Hello';
 import Hobby from './components/Hobby/Hobby';
 
 function App() {
+  const [counterLeft, setCounterLeft] = useState(0)
+  const [counterRight, setCounterRight] = useState(0)
+  const [counterHistory, setCounterHistory] = useState('')
+
+  const leftClick = () => {
+    setCounterLeft(counterLeft + 1)
+    setCounterHistory(counterHistory + ' L')
+  }
+  
+  const rightClick = () => {
+    setCounterRight(counterRight + 1)
+    setCounterHistory(counterHistory + ' R')
+  }
+
+
+  // const [counter, setCounter] = useState(0); //[state, setFn]
+  // const handleClick = () => {
+  //   setCounter(counter + 1)
+  // }
+  // const decreaseByOne = () => {
+  //   setCounter(counter - 1)
+  // } 
+  // const changeToZero = () => {
+  //   setCounter(0)
+  // }
   return (
     <div className='App'>
-      <Header />   
+      <Header />
+      <p style={{ fontSize: '40px' }}>Клик левой кнопки {counterLeft}</p>
+      <p style={{ fontSize: '40px' }}>Клик правой кнопки {counterRight}</p>
+      <button onClick={leftClick}>Клик левой</button>
+      <button onClick={rightClick}>Клик правой</button>
+      <h1>История кликов {counterHistory}</h1>
+
+
+      {/* <p style={{ fontSize: '60px' }}>{counter}</p>
+      <button onClick={handleClick}>Увеличить на 1</button>
+      <button onClick={decreaseByOne}>Уменьшить на 1</button>
+      <button onClick={changeToZero}>Reset</button> */}
+
+
       <Hello name='Maral' age='28' />
-      <Hobby arts='painting, reading'/>
+      <Hobby arts='painting, reading' />
       <Hello name='Max' age='25' />
       <Hobby sports='camping, swimming' />
       <Hello name='Alice' age='24' />
@@ -24,6 +63,4 @@ export default App;
 // Feature Slices
 
 // Компонент - функция, которая возвращает JSX (HTML+JS) разметку
-
 // Декомпозиция - разделение кода на компоненты
-
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>Приветствие</h1> 
+      <h1>Hello</h1> 
     </header>
   );
 };
